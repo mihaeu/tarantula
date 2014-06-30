32692aff33974c65d8eb2a36f3ee0e81a1d42398
Michael Haeuslmann <haeuslmann@gmail.com>
2014-06-30 22:20:46 +0200

    Refactored tests that hit io.

--- tests/Mihaeu/Tarantula/Action/MirrorResultActionTest.php
+++ tests/Mihaeu/Tarantula/Action/MirrorResultActionTest.php
@@ -9,35 +9,14 @@ class MirrorResultActionTest extends PHPUnit_Framework_TestCase
     {
         $result = new Result('', 'https://google.com/test/site.html', '<wayne>');
         $testFolder = sys_get_temp_dir().DIRECTORY_SEPARATOR.'phpunit-'.date('Y-m-d-H-i-s').rand();
-        mkdir($testFolder);
+        $fs = new Symfony\Component\Filesystem\Filesystem($testFolder);
+        $fs->mkdir($testFolder);
 
         $action = new MirrorResultAction($testFolder);
-        $processedResult = $action->execute($result);
+        $action->execute($result);
         $this->assertEquals('<wayne>', file_get_contents($testFolder.DIRECTORY_SEPARATOR.'google.com/test/site.html'));
 
-        $this->rrmdir($testFolder);
+        $fs->remove($testFolder);
         $this->assertFalse(file_exists($testFolder));
-    }
-
-    /**
-     * Recursively removes a folder along with all its files and directories.
-     *
-     * Taken from: http://ben.lobaugh.net/blog/910/php-recursively-remove-a-directory-and-all-files-and-folder-contained-within
-     * 
-     * @param String $path 
-     *
-     * @author Ben Lobaugh
-     */
-    private function rrmdir($path) {
-        // Open the source directory to read in files
-        $i = new DirectoryIterator($path);
-        foreach($i as $f) {
-            if($f->isFile()) {
-                unlink($f->getRealPath());
-            } else if(!$f->isDot() && $f->isDir()) {
-                $this->rrmdir($f->getRealPath());
-            }
-        }
-        rmdir($path);
     }
 }
--- tests/Mihaeu/Tarantula/Action/SaveHashedResultActionTest.php
+++ tests/Mihaeu/Tarantula/Action/SaveHashedResultActionTest.php
@@ -9,38 +9,17 @@ class SaveHashedResultActionTest extends PHPUnit_Framework_TestCase
     {
         $result = new Result('0123456789', 'http://example.com', '<html>');
         $testFolder = sys_get_temp_dir().DIRECTORY_SEPARATOR.'phpunit-'.date('Y-m-d-H-i-s').rand();
-        mkdir($testFolder);
-        
+        $fs = new Symfony\Component\Filesystem\Filesystem($testFolder);
+        $fs->mkdir($testFolder);
+
         $action = new SaveHashedResultAction($testFolder);
-        $processedResult = $action->execute($result);
+        $action->execute($result);
 
         $savedFile = $testFolder.DIRECTORY_SEPARATOR.'0'.DIRECTORY_SEPARATOR.'0123456789';
         $this->assertTrue(file_exists($savedFile));
         $this->assertEquals('<html>', file_get_contents($savedFile));
-        
-        $this->rrmdir($testFolder);
-        $this->assertFalse(file_exists($testFolder));
-    }
 
-    /**
-     * Recursively removes a folder along with all its files and directories.
-     *
-     * Taken from: http://ben.lobaugh.net/blog/910/php-recursively-remove-a-directory-and-all-files-and-folder-contained-within
-     * 
-     * @param String $path 
-     *
-     * @author Ben Lobaugh
-     */
-    private function rrmdir($path) {
-        // Open the source directory to read in files
-        $i = new DirectoryIterator($path);
-        foreach($i as $f) {
-            if($f->isFile()) {
-                unlink($f->getRealPath());
-            } else if(!$f->isDot() && $f->isDir()) {
-                $this->rrmdir($f->getRealPath());
-            }
-        }
-        rmdir($path);
+        $fs->remove($testFolder);
+        $this->assertFalse(file_exists($testFolder));
     }
 }
