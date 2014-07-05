cfe53a4d925d947da4119d7066d28cfde8ad4db0
Michael Haeuslmann <haeuslmann@gmail.com>
2014-07-05 12:32:46 +0200

    Fixed naming conflict and brought back mirror command.

--- src/Mihaeu/Tarantula/Action/MirrorResultAction.php
+++ src/Mihaeu/Tarantula/Action/MirrorResultAction.php
@@ -7,7 +7,7 @@ use Mihaeu\Tarantula\Result;
 use Symfony\Component\Filesystem\Filesystem;
 
 /**
- * Save Hased Result Action
+ * Mirror Result Action
  *
  * @author Michael haeuslmann <haeuslmann@gmail.com>
  */
@@ -42,11 +42,42 @@ class MirrorResultAction implements ActionInterface
      */
     public function execute(Result $result)
     {
-        $filename = preg_replace('/^((https?):\/\/)?(www.)?/', '', $result->getLink());
-        $saveTo = $this->dir.DIRECTORY_SEPARATOR.$filename;
-        $fs = new Filesystem($saveTo);
+        // strip protocol and www.
+        $url = preg_replace('/^((https?):\/\/)?(www\d{0,3}\.)?/', '', $result->getLink());
+
+        // URLs like blog.com/posts and blog.com/posts/a would conflict, because
+        // the first URL would be created as a file and the same name cannot be used for
+        // a directory with the same name. Workaround is to simply attach an underscore
+        // to the *index* file.
+        if ($this->isPrettyUrl($url)) {
+            $url = $url.'_';
+        }
+        $saveTo = $this->dir.DIRECTORY_SEPARATOR.$url;
+        $fs = new Filesystem();
         $fs->dumpFile($saveTo, $result->getData());
 
         return $result;
+    }
+
+    /**
+     * Checks is a URL is pretty.
+     *
+     * Pretty URLs contain no file endings like `.php`. The check is not
+     * 100%, but checking for the usual file ending patterns should do.
+     * Plain domain names are always pretty.
+     *
+     * @param  String $url
+     *
+     * @return bool
+     */
+    public function isPrettyUrl($url)
+    {
+//        $regex = '/
+//            .*\/            # get as many characters up to the last /
+//            [^\.]+          # move forward till the last .
+//            \.\w{2,4}$      # check if there is a file ending at the end
+//            /x';
+        $regex = '/.*\/[^\.]+\.\w{2,4}$/';
+        return 1 !== preg_match($regex, $url);
     }
 }
--- tests/Mihaeu/Tarantula/Action/MirrorResultActionTest.php
+++ tests/Mihaeu/Tarantula/Action/MirrorResultActionTest.php
@@ -15,7 +15,7 @@ class MirrorResultActionTest extends PHPUnit_Framework_TestCase
         foreach ($urls as $url) {
             $result = new Result('', $url, '<wayne>');
             $testFolder = sys_get_temp_dir().DIRECTORY_SEPARATOR.'phpunit-'.date('Y-m-d-H-i-s').rand();
-            $fs = new Symfony\Component\Filesystem\Filesystem($testFolder);
+            $fs = new Symfony\Component\Filesystem\Filesystem();
             $fs->mkdir($testFolder);
 
             $action = new MirrorResultAction($testFolder);
@@ -23,5 +23,17 @@ class MirrorResultActionTest extends PHPUnit_Framework_TestCase
             $this->assertEquals('<wayne>', file_get_contents($testFolder.DIRECTORY_SEPARATOR.'google.com/test/site.html'));
             $fs->remove($testFolder);
         }
+    }
+
+    public function testDetectsPrettyUrls()
+    {
+        $action = new MirrorResultAction(sys_get_temp_dir());
+
+        $this->assertTrue($action->isPrettyUrl('google.com'));
+        $this->assertTrue($action->isPrettyUrl('google.com/posts'));
+        $this->assertTrue($action->isPrettyUrl('google.com/posts/too-crazy-and.com/deep'));
+
+        $this->assertFalse($action->isPrettyUrl('http://google.com'));
+        $this->assertFalse($action->isPrettyUrl('google.com/my/deep/url.php'));
     }
 }
