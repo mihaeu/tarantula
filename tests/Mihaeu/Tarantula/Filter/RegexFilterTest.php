cdbbd345361020c3ef5aec2ceebfd8ae48b38163
Michael Haeuslmann <haeuslmann@gmail.com>
2014-07-05 12:33:07 +0200

    Added more test cases.

--- tests/Mihaeu/Tarantula/Filter/RegexFilterTest.php
+++ tests/Mihaeu/Tarantula/Filter/RegexFilterTest.php
@@ -9,5 +9,9 @@ class RegexFilterTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue($filter->filter('https://example.com/posts/test.php'));
         $this->assertTrue($filter->filter('example.com/posts/1231'));
+
+        $filter = new Mihaeu\Tarantula\Filter\RegexFilter('/(part)|(\d+)/');
+        $this->assertTrue($filter->filter('http://mike-on-a-bike.com/catching-up-part-3'));
+        $this->assertTrue($filter->filter('http://mike-on-a-bike.com/100-days-on-the-road'));
     }
 }
