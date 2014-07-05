d3431555f8123bac608f76d8f52e377c2a4096c3
Michael Haeuslmann <haeuslmann@gmail.com>
2014-07-05 22:14:51 +0200

    Added XPath action.

---
+++ src/Mihaeu/Tarantula/Action/XPathTextAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Mihaeu\Tarantula\Action;
+
+use Mihaeu\Tarantula\Result;
+
+use Symfony\Component\DomCrawler\Crawler as DOMCrawler;
+
+/**
+ * XPath Text Action
+ *
+ * @author Michael haeuslmann <haeuslmann@gmail.com>
+ */
+class XPathTextAction implements ActionInterface
+{
+    /**
+     * @var String
+     */
+    private $xpath;
+
+    /**
+     * Constructor.
+     *
+     * @param String $xpath
+     */
+    public function __construct($xpath)
+    {
+        $this->xpath = $xpath;
+    }
+
+    /**
+     * Return text that matches a XPath expression.
+     *
+     * @param  Result $result
+     *
+     * @return Result
+     */
+    public function execute(Result $result)
+    {
+        $domCrawler = new DOMCrawler();
+        $domCrawler->addContent($result->getData());
+
+        $domCrawler->filterXPath($this->xpath)->each(
+            function ($node) {
+                $text = trim($node->text());
+                if (!empty($text)) {
+                    echo $text.PHP_EOL;
+                }
+            }
+        );
+
+        return $result;
+    }
+}
