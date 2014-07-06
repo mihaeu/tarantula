aad2909b0e495487d554352a2eeca95e867b1e37
Michael Haeuslmann <haeuslmann@gmail.com>
2014-07-06 20:24:47 +0200

    Process now async.

--- tests/unit/Tests/BaseUnitTest.php
+++ tests/unit/Tests/BaseUnitTest.php
@@ -16,16 +16,38 @@ abstract class BaseUnitTest extends \PHPUnit_Framework_TestCase
      */
     protected $demoServerProcess;
 
+    /**
+     * @var string
+     */
+    protected $host = 'localhost:9137';
+
+    /**
+     * Starts the internal PHP server using excerpts from my (awesome)
+     * travel blog as the docroot.
+     */
     public function startDemoServer()
     {
-        $docRoot = realpath(__DIR__ . '/../../../../demo');
-        $this->demoServerProcess = new Process('php -S localhost:9134 -t ' . $docRoot);
+        $docRoot = realpath(__DIR__ . '/../../demo/mike-on-a-bike.com');
+        $this->demoServerProcess = new Process("php -S $this->host -t $docRoot");
         $this->demoServerProcess->setTimeout(3600);
-        $this->demoServerProcess->run();
+        $this->demoServerProcess->start();
     }
 
+    /**
+     * Stops the server.
+     */
     public function stopDemoServer()
     {
         $this->demoServerProcess->stop();
+    }
+
+    /**
+     * Returns the demo url that is currently used for the server.
+     *
+     * @return string
+     */
+    public function getDemoUrl()
+    {
+        return 'http://'.$this->host;
     }
 }
